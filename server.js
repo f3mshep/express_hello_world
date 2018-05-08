c90c5ad6c30e98b0811a35071c3fe7a696c39013
f3mshep <superbiscuit@gmail.com>
2018-05-08 11:00:54 -0700

    added server construction status toggle

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs')
 const app = express();
-
+CONSTRUCTION = false
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', ()=> new Date().getFullYear())
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log)
+  fs.appendFile('server.log', log + "\n", (err)=> {
+    if (err){
+      console.log('Could not write to server log.');
+    };
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  if (CONSTRUCTION) res.render("repair.hbs")
+  else next()
+});
+
+hbs.registerHelper('getCurrentYear', ()=> new Date().getFullYear());
 hbs.registerHelper('screamIt', (text)=> text.toUpperCase());
 
 app.get('/', (req, res)=> {
